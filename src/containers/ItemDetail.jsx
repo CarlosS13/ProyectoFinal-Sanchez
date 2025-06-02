cf9dd016810666e42a0131b9f88ef45add2c6ae8
CarlosS13 <Carloscaprari36@gmail.com>
2025-06-02 19:42:41 -0300

    Entrega casi finalizada

--- src/containers/ItemDetail.jsx
+++ src/containers/ItemDetail.jsx
@@ -6,7 +6,7 @@ const ItemDetail = ({ nombre, descripcion, valor }) => {
       <h2>{nombre}</h2>
       <p>{descripcion}</p>
       <p>Precio: ${valor}</p>
-      <ItemCount stock={10} inicial={1} onAdd={(cantidad) => console.log(`Agregado: ${cantidad}`)} />
+      <ItemCount stock={10} inicial={1}  />
     </div>
   );
 };
